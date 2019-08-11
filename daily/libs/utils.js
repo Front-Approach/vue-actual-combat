12a40bbcf08bd97eb564b1751013004d5a179f11
Minghui.Zhou <minghui.zhou@jointwisdom.cn>
2019-08-12 00:42:03 +0800

    node代理和axios配置

---
+++ daily/libs/utils.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+// 基本配置
+const Util = {
+    imgPath: 'http://127.0.0.1:8011/img/',
+    apiPath: 'http://127.0.0.1:8010;'
+};
+
+// ajax通用配置
+Util.ajax = axios.create({
+    basicURL: Util.apiPath
+});
+
+// 添加相应拦截器
+Util.ajax.interceptors.reponse.use(res => {
+    return res.data;
+})
+
+export default Util;
